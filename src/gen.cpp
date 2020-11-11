fe61a00158e7cb04165179cb307568d1e6ea6c0b
Albert Skalt <allber971@gmail.com>
2020-11-11 18:22:59 +0300

    Delete trash

--- src/gen.cpp
+++ src/gen.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
 
+#define sz(a) (int) a.size()
+#define all(a) a.begin(), a.end()
+#define ll long long
+#define ld long double
+
 using namespace std;
+mt19937_64 rnd(std::chrono::system_clock::now().time_since_epoch().count());
 
 int main() {
-    cout << 3;
+    ios::sync_with_stdio(false), cin.tie(0);
+    cout.setf(ios::fixed), cout.precision(10);
+    return 0;
 }
