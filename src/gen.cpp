7c30120e8adb044e701aedc83d25f17953539e22
Albert Skalt <allber971@gmail.com>
2020-11-17 10:02:00 +0300

    Edit source (cstress print test)

--- src/gen.cpp
+++ src/gen.cpp
@@ -3,13 +3,15 @@
 #define sz(a) (int) a.size()
 #define all(a) a.begin(), a.end()
 #define ll long long
-#define ld long double
+#define ld double
 
 using namespace std;
 mt19937_64 rnd(std::chrono::system_clock::now().time_since_epoch().count());
 
 int main() {
-    ios::sync_with_stdio(false), cin.tie(0);
+    ios::sync_with_stdio(false), cin.tie(nullptr);
     cout.setf(ios::fixed), cout.precision(10);
+
+
     return 0;
 }
